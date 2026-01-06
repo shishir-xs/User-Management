b597b70365d9ef55131e06a533709e3b256e630d
Shishir Bhuiyan <shishir_b@roxnor.com>
2026-01-06 10:54:23 +0600

    Shift Roxnor User Management into a singleton arch.

--- wp-content/plugins/roxnor-user/roxnor-user.php
+++ wp-content/plugins/roxnor-user/roxnor-user.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Roxnor User Management
  * Description: Roxnor User Management Plugin.
@@ -10,21 +11,39 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
-final class Roxnor_User_Management {
-   const VERSION = '1.0';
+final class Roxnor_User_Management
+{
+    private static $instance = null;
+    const VERSION = '1.0';
 
 
-   private function __construct() {
-      $this->define_constants();
-   }
+    private function __construct()
+    {
+        $this->define_constants();
+    }
 
-   public function define_constants() {
-       if ( ! defined( 'ROXNOR_USER_VERSION' ) ) {
-           define( 'ROXNOR_USER_VERSION', self::VERSION );
-       }
-   }
+    public function define_constants(): void
+    {
+        if (! defined('ROXNOR_USER_VERSION')) {
+            define('ROXNOR_USER_VERSION', self::VERSION);
+        }
+    }
+    
+    public static function init(): Roxnor_User_Management
+    {
+        if (! self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 }
+
+function roxnor_user_management(): Roxnor_User_Management
+{
+    return Roxnor_User_Management::init();
+}
+roxnor_user_management();
