7ebf7537335b87c7635518684d8821995cc5b89b
Shishir Bhuiyan <shishir_b@roxnor.com>
2026-01-06 11:58:01 +0600

    feat(backend): register User Management top-level admin menu and add Dashboard & Import & Export submenus

--- wp-content/plugins/roxnor-user/src/Backend/Menu.php
+++ wp-content/plugins/roxnor-user/src/Backend/Menu.php
@@ -8,16 +8,41 @@ class Menu {
     }
 
     public function add_menu() {
-        add_users_page(
+        $parent_slug = 'roxnor-user-management';
+        $capability = 'manage_options';
+
+        add_menu_page(
             'User Management',
             'User Management',
-            'manage_options',
-            'user-management',
+            $capability,
+            $parent_slug,
             [ $this, 'render_page' ]
+        );
+
+        add_submenu_page(
+            $parent_slug,
+            'Dashboard',
+            'Dashboard',
+            $capability,
+            $parent_slug,
+            [ $this, 'render_page' ]
+        );
+
+        add_submenu_page(
+            $parent_slug,
+            'Import & Export',
+            'Import & Export',
+            $capability,
+            'roxnor-import-export',
+            [ $this, 'render_import_export' ]
         );
     }
 
     public function render_page() {
         echo '<h1>Hello This is RoxNor User Management Page !</h1>';
+    }
+
+    public function render_import_export() {
+        echo '<h1>Hello This is RoxNor Import & Export Page !</h1>';
     }
 }
