155708558ec03fb2e96b9ef6c473579d769af21f
Shishir Bhuiyan <shishir_b@roxnor.com>
2026-01-06 09:55:32 +0600

    plugin header

---
+++ wp-content/plugins/roxnor-user/roxnor-user.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Roxnor User Management
+ * Description: Roxnor User Management Plugin.
+ * Plugin URI: https://github.com/shishir-xs
+ * Author: Tareq Hasan
+ * Author URI: https://github.com/shishir-xs
+ * Version: 1.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
