89db96ab645f90613bdb3709caf3eb2e4435398c
Shishir Bhuiyan <shishir_b@roxnor.com>
2026-01-06 18:01:15 +0600

    refactor(backend): enhance Backend class structure and initialization logic

--- wp-content/plugins/roxnor-user/src/Backend.php
+++ wp-content/plugins/roxnor-user/src/Backend.php
@@ -2,8 +2,21 @@
 
 namespace Roxnor\UserManagement;
 
+use Roxnor\UserManagement\Backend\Assets;
+
 class Backend {
+    private $container;
+    
     public function __construct() {
+        $this->container = Container::getInstance();
+        
         new Backend\Menu();
+        new Assets();
+        
+        register_activation_hook(ROXNOR_USER_FILE, [$this, 'createTables']);
+    }
+    
+    public function createTables() {
+        $this->container->getUserRepository()->createTable();
     }
 }
